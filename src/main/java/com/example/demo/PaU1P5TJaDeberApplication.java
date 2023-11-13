dc6182f2a71452508353042061a0ba6dc5a2b694
Jhonjaxde <jjarteaga@uce.edu.ec>
2023-11-13 14:28:13 -0500

    branch original

---
+++ src/main/java/com/example/demo/PaU1P5TJaDeberApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaU1P5TJaDeberApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaU1P5TJaDeberApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("original");
+		
+	}
+
+}
